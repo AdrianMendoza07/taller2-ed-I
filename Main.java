086e63fb2d07108a128406c32e0b182399cf3177
Nataly Lopez <napologo1010@gmail.com>
2026-02-25 16:53:20 +0000

    registre cliente y liste cliente

--- Main.java
+++ Main.java
@@ -1,17 +1,31 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Elija su opcion:");
         int opcion = sc.nextInt();
 
-        while(opcion != 6){
+        while (opcion != 6) {
 
             switch (opcion) {
-                case 6:
-                    
+                case 1:
+                    System.out.println("Ingrese su ID: ");
+                    id = sc.nextLine();
+                    System.out.println("Ingrese su Nombre: ");
+                    name = sc.nextLine();
+                    System.out.println("Ingrese su Apellido: ");
+                    lastname = sc.nextLine();
+                    System.out.println("Ingrese su Numero de celular: ");
+                    phone = sc.nextLine();
+                    cliente cliente = new cliente();
+                    cliente.Usuario(id, name, lastname, phone, true);
+
+                    registrarCliente(cliente);
+                    break;
+                case 2:
+                    leerClientes();
                     break;
                 default:
                     throw new AssertionError();
--- manejoArchivos.java
+++ manejoArchivos.java
@@ -3,4 +3,32 @@ public class manejoArchivos {
 
     private static final String FILE = "clintes.csv";
 
+    public static void registrarCliente(cliente cliente)
+            throws IOException {
+        FileWriter fw = new FileWriter("clientes.csv", true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(cliente.toString());
+        bw.newLine();
+        bw.close();
+
+    }
+
+    public static List<cliente> leerClientes()
+            throws IOException {
+        List<cliente> lista = new ArrayList<>();
+        Scanner sc = new Scanner(new File("clientes.csv"));
+
+        while (sc.hasNextLine()) {
+            String[] datosc = sc.nextLine().split(",");
+            lista.add(new cliente(
+                    Integer.parseInt(datosc[0]),
+                    datosc[1],
+                    datosc[2],
+                    datosc[3],
+                    datosc[4]));
+        }
+        sc.close();
+        return lista;
+    }
+
 }
